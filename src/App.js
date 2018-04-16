efd1d8571ab7803233abbe6f8de41ebf66c60c7c
davidmoradi <david@wishpond.com>
2018-04-16 16:39:20 -0700

    Preparation for allowing players to enter the lottery

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3'
 import lottery from './lottery';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { manager: '' };
+    this.state = {
+      manager: '',
+      players: [],
+      balance: '',
+      value: ''
+    };
   }
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address)
 
-    this.setState({ manager: manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <h2> Lottery Contract </h2>
-        <p> This contract is managed by {this.state.manager} </p>
+        <p>
+          This contract is managed by {this.state.manager}
+        </p>
+        <p>
+          There are currently {this.state.players.length} people entered this lottery
+          to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+
+        <form>
+          <h4> Want to try your luck ? </h4>
+          <div>
+            <label> Amount of ether to enter </label>
+            <input
+              value={this.state.value}
+              onChange={ event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
